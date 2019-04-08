1c7e18491e7aa9a956054e5a0da263a2f3c0c0d5
Jonathan B <bluxget@gmail.com>
2019-04-08 11:34:52 +0200

    Add cors middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const logger = require('morgan');
 const fs = require('fs');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const sendResponse = require('./statusCode');
 const security = require('./security');
@@ -21,6 +22,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(helmet());
 app.disable('x-powered-by');
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
